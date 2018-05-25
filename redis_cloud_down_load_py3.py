7ffc63d46e306db080770b5f810bc04674105480
Glenn Edgar <glenn-edgar@onxyengr.com>
2018-05-25 19:10:44 +0000

    daily update

--- redis_cloud_down_load_py3.py
+++ redis_cloud_down_load_py3.py
@@ -23,23 +23,24 @@ class REDIS_CLOUD_SYNC(object):
 
   
        self.client = mqtt.Client(client_id="", clean_session=True, userdata=None,  transport="tcp")
-       self.client.tls_set(certfile= "/home/pi/mosquitto/certs/client.crt", keyfile= "/home/pi/mosquitto/certs/client.key", cert_reqs=ssl.CERT_NONE )
+       self.client.tls_set(certfile= "mosquitto_keys/server.crt", 
+                           keyfile= "mosquitto_keys/server.key", cert_reqs=ssl.CERT_NONE )
        
        redis_handle_pw = redis.StrictRedis(redis_site_data["host"], 
                                            redis_site_data["port"], 
                                            db=redis_site_data["redis_password_db"], 
                                            decode_responses=True)
                                         
-       self.client.username_pw_set("remote", redis_handle_pw.hget("mosquitto_cloud","remote"))
+       self.client.username_pw_set("cloud", redis_handle_pw.hget("mosquitto_local","cloud"))
        
        self.client.on_connect = self.on_connect
        self.client.on_message = self.on_message
-       self.client.connect(redis_site_data["mqtt_cloud_server"],redis_site_data["mqtt_cloud_port"], 60)
+       self.client.connect(redis_site_data["mqtt_server"],redis_site_data["mqtt_port"], 60)
        self.client.loop_forever()
 
    def on_connect(self,client, userdata, flags, rc):
        print("Connected with result code "+str(rc),self.redis_site_data["mqtt_topic"])
-       self.client.subscribe(self.redis_site_data["mqtt_cloud_topic"])
+       self.client.subscribe(self.redis_site_data["mqtt_topic"])
 
 
    def on_message(self, client, userdata, msg):
